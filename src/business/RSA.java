bf7bfaf02e0e449265d1255ef5a0174172ec5bf2
DrakeNguyenDuy <ndlong28@gmail.com>
2022-10-23 15:42:39 +0700

    add file readme

--- src/business/RSA.java
+++ src/business/RSA.java
@@ -10,7 +10,6 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.swing.JOptionPane;
 
 import helppers.Constants;
 
@@ -37,37 +36,35 @@ public class RSA {
 
 	public String encrypt(byte[] byteText, String keyType, Object... objects)
 			throws IllegalBlockSizeException, InvalidKeyException, ClassCastException, BadPaddingException {
-		
-			switch (keyType) {
-			case Constants.PRIVATE_KEY:
-				cipher.init(Cipher.ENCRYPT_MODE, (PrivateKey) objects[0]);
-				break;
-			default:
-				cipher.init(Cipher.ENCRYPT_MODE, (PublicKey) objects[0]);
-				break;
-			}
-			byte[] byteResult = cipher.doFinal(byteText);
-			System.out.println("Mã hóa: " + Base64.getEncoder().encodeToString(byteResult) + "==");
-			return Base64.getEncoder().encodeToString(byteResult);
-		
+		switch (keyType) {
+		case Constants.PRIVATE_KEY:
+			cipher.init(Cipher.ENCRYPT_MODE, (PrivateKey) objects[0]);
+			break;
+		default:
+			cipher.init(Cipher.ENCRYPT_MODE, (PublicKey) objects[0]);
+			break;
+		}
+		byte[] byteResult = cipher.doFinal(byteText);
+		System.out.println("Mã hóa: " + Base64.getEncoder().encodeToString(byteResult) + "==");
+		return Base64.getEncoder().encodeToString(byteResult);
+
 	}
 
 	public byte[] decrypt(String byteText, String keyType, Object... objects)
 			throws IllegalBlockSizeException, InvalidKeyException, ClassCastException, BadPaddingException {
-		
-			switch (keyType) {
-			case Constants.PUBLIC_KEY:
-				cipher.init(Cipher.DECRYPT_MODE, (PublicKey) objects[0]);
-				break;
-			default:
-				cipher.init(Cipher.DECRYPT_MODE, (PrivateKey) objects[0]);
-				break;
-			}
-			byte[] temp = Base64.getDecoder().decode(byteText);
-			byte[] byteResult = cipher.doFinal(temp);
-			System.out.println("Sau Mã hóa: " + new String(byteResult));
+		switch (keyType) {
+		case Constants.PUBLIC_KEY:
+			cipher.init(Cipher.DECRYPT_MODE, (PublicKey) objects[0]);
+			break;
+		default:
+			cipher.init(Cipher.DECRYPT_MODE, (PrivateKey) objects[0]);
+			break;
+		}
+		byte[] temp = Base64.getDecoder().decode(byteText);
+		byte[] byteResult = cipher.doFinal(temp);
+		System.out.println("Sau Mã hóa: " + new String(byteResult));
 //			return Base64.getEncoder().encodeToString(byteResult);
-			return byteResult;
+		return byteResult;
 	}
 
 //	public static void main(String[] args) throws IllegalBlockSizeException {
