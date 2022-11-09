173d18947da718edcff1f3db5fec0a71d66c9a1d
DrakeNguyenDuy <ndlong28@gmail.com>
2022-11-10 00:48:04 +0700

    MD5 SHA1 SHA256

---
+++ src/business/Hashing.java
@@ -0,0 +1,184 @@
+package business;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import helppers.Constants;
+
+public class Hashing {
+	private List<String> storeHasing;// list contain hashing code
+	private List<String> storePt;// list contain plain text
+	private MessageDigest hashing;
+
+	public Hashing() {
+		this.storeHasing = new ArrayList<>();
+	}
+
+	/*encrypt plain text to hashing code
+	 *input: 
+	 *+ typeHashing: Type hashing
+	 *+ plaintText: data input
+	 *+ append: is save in store
+	 *output: hashing code 
+	 */
+	public String encrypt(String typeHasing, byte[] plainText, boolean append) {
+		try {
+			this.hashing = MessageDigest.getInstance(typeHasing);
+			this.hashing.update(plainText);
+			byte[] digest = this.hashing.digest();
+			BigInteger bigInteger = new BigInteger(1, digest);
+			String hash = bigInteger.toString(16);// hex value
+			if (append) {
+				String pathStoreHashing = null;
+				String pathStorePlaintext = null;
+				/*
+				 * get path of store hashing code
+				 * and path of store plain text
+				 */
+				switch (typeHasing) {
+				case Constants.MD5:
+					pathStoreHashing = Constants.pathHashMD5;
+					pathStorePlaintext = Constants.pathPlTextMD5;
+					break;
+				case Constants.SHA1:
+					pathStoreHashing = Constants.pathHashSHA1;
+					pathStorePlaintext = Constants.pathPlTextSHA1;
+					break;
+				case Constants.SHA256:
+					pathStoreHashing = Constants.pathHashSHA256;
+					pathStorePlaintext = Constants.pathPlTextSHA256;
+					break;
+				default:
+					break;
+				}
+				addHashToStore(pathStoreHashing, hash);
+				addPTToStore(pathStorePlaintext, new String(plainText));
+			}
+			return hash;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	
+	//add hashing code to store hashing
+	public void addHashToStore(String pathStoreHash, String newHash) {
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(pathStoreHash), true));
+			CharSequence charSequence = new String(newHash);
+			bufferedWriter.append(charSequence);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//add plaint text to store plaint text
+	public void addPTToStore(String pathPlTextStore, String pt) {
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(pathPlTextStore), true));
+			CharSequence charSequence = new String(pt);
+			bufferedWriter.append(charSequence);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//load hashing from store
+	public void loadHashing(String typeHashing) {
+		try {
+			String path = null;
+			//get path by type hashing
+			switch (typeHashing) {
+			case Constants.MD5:
+				path = Constants.pathHashMD5;
+				break;
+			case Constants.SHA1:
+				path = Constants.pathHashSHA1;
+				break;
+			case Constants.SHA256:
+				path = Constants.pathHashSHA256;
+				break;
+			default:
+				break;
+			}
+			//load hashing
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(path)));
+			Stream<String> lines = bufferedReader.lines();
+			lines.distinct().forEach(item -> this.storeHasing.add(item));
+			lines.close();
+			bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void loadPT(String typeHashing) throws IOException {
+		try {
+			String path = null;
+			//get path store plant text
+			switch (typeHashing) {
+			case Constants.MD5:
+				path = Constants.pathPlTextMD5;
+				break;
+			case Constants.SHA1:
+				path = Constants.pathPlTextSHA1;
+				break;
+			case Constants.SHA256:
+				path = Constants.pathPlTextSHA256;
+				break;
+			default:
+				break;
+			}
+			//load plant text 
+			this.storePt = new ArrayList<>();
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(path)));
+			Stream<String> lines = bufferedReader.lines();
+			lines.distinct().forEach(item -> this.storePt.add(item));
+			lines.close();
+			bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//decrypt is load hashing and plait text stored 
+	public String decrypt(String hashingCode, String typeHashing) {
+		//load all hashing stored
+		loadHashing(typeHashing);
+		//check is contain hashing code finding
+		if (storeHasing.contains(hashingCode)) {
+			try {
+				//load plain text by hashing code
+				loadPT(typeHashing);
+				int index = storeHasing.indexOf(hashingCode);
+				String pt = this.storePt.get(index);
+				return pt;
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		//not found hashing code
+		return "Not found";
+	}
+}
--- src/presentations/SHA256Presentation.java
+++ src/presentations/SHA256Presentation.java
@@ -3,22 +3,35 @@ package presentations;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+
+import business.Hashing;
+import helppers.Constants;
+import helppers.ReadFile;
+import helppers.WriteFile;
+
 import java.awt.Color;
 
-public class SHA256Presentation extends JPanel {
-
+public class SHA256Presentation extends JPanel implements IPresentation{
+	private ButtonGroup buttonGroup;
+	private JTextArea taText;
+	private JTextArea taResult;
+	private Hashing hashing;
 	/**
 	 * Create the panel.
 	 */
 	public SHA256Presentation() {
+		this.hashing = new Hashing();
 		setBorder(new EmptyBorder(0, 5, 5, 0));
 		setSize(550, 450);
 		setLayout(null);
@@ -33,20 +46,20 @@ public class SHA256Presentation extends JPanel {
 		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		panel.add(lblNewLabel);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(0, 24, 229, 200);
-		textArea.setLineWrap(true);
-		panel.add(textArea);
+		taText = new JTextArea();
+		taText.setBounds(0, 24, 229, 200);
+		taText.setLineWrap(true);
+		panel.add(taText);
 		
 		JLabel lblNewLabel_1 = new JLabel("Your result");
 		lblNewLabel_1.setBounds(0, 235, 100, 14);
 		panel.add(lblNewLabel_1);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(0, 250, 229, 200);
-		textArea_1.setLineWrap(true);
-		textArea_1.setColumns(10);
-		panel.add(textArea_1);
+		taResult = new JTextArea();
+		taResult.setBounds(0, 250, 229, 200);
+		taResult.setLineWrap(true);
+		taResult.setColumns(10);
+		panel.add(taResult);
 
 		
 		JPanel panel_1 = new JPanel();
@@ -61,38 +74,83 @@ public class SHA256Presentation extends JPanel {
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Encrypt");
 		rdbtnNewRadioButton.setBounds(0, 21, 111, 23);
 		panel_1.add(rdbtnNewRadioButton);
+		rdbtnNewRadioButton.setSelected(true);
+		rdbtnNewRadioButton.setActionCommand(Constants.ENCRYPT);
 		
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Decrypt");
 		rdbtnNewRadioButton_1.setBounds(115, 21, 111, 23);
 		panel_1.add(rdbtnNewRadioButton_1);
+		rdbtnNewRadioButton_1.setActionCommand(Constants.DECRYPT);
+		buttonGroup = new ButtonGroup();
+		buttonGroup.add(rdbtnNewRadioButton);
+		buttonGroup.add(rdbtnNewRadioButton_1);
 		
-		JButton btnNewButton = new JButton("Import Text");
+		JButton btnNewButton = new JButton("Import File");
 		btnNewButton.setForeground(SystemColor.text);
 		btnNewButton.setBackground(SystemColor.textHighlight);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					loadText(null);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		});
 		btnNewButton.setBounds(0, 51, 105, 23);
 		panel_1.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("Import Hash");
+		JButton btnNewButton_1 = new JButton("Save Result");
 		btnNewButton_1.setForeground(Color.WHITE);
 		btnNewButton_1.setBackground(SystemColor.textHighlight);
 		btnNewButton_1.setBounds(115, 51, 105, 23);
 		panel_1.add(btnNewButton_1);
-		
-		JButton btnNewButton_1_1 = new JButton("Save Hash");
-		btnNewButton_1_1.setForeground(Color.WHITE);
-		btnNewButton_1_1.setBackground(SystemColor.textHighlight);
-		btnNewButton_1_1.setBounds(69, 102, 105, 23);
-		panel_1.add(btnNewButton_1_1);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				saveFile(null);
+			}
+		});
 		
 		JButton btnNewButton_2 = new JButton("Start");
 		btnNewButton_2.setBounds(69, 305, 105, 23);
 		panel_1.add(btnNewButton_2);
-		
+		btnNewButton_2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				execute();
+			}
+		});
 
 	}
+	@Override
+	public void execute() {
+		String mode = buttonGroup.getSelection().getActionCommand();
+		String input = taText.getText();
+		String result = "";
+		if (mode.equals(Constants.ENCRYPT)) {
+			result = this.hashing.encrypt(Constants.SHA256, input.getBytes(), true);
+		} else {
+			result = this.hashing.decrypt(input, Constants.SHA256);
+		}
+		taResult.setText(result);
+	}
 
+	@Override
+	public void loadText(String type) throws IOException {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.showOpenDialog(this);
+		String path = fileChooser.getCurrentDirectory() + "\\" + fileChooser.getSelectedFile().getName();
+		byte[] hashByte = ReadFile.getInstance().readFile(path, null);
+		taText.setText(new String(hashByte));
+	}
+
+	@Override
+	public void saveFile(String type) {
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.showSaveDialog(this);
+		String path = fileChooser.getCurrentDirectory() + "\\" + fileChooser.getSelectedFile().getName();
+		WriteFile.getInstance().writeFile(path, taResult.getText().getBytes());
+	}
 }
